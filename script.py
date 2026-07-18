82d6de267f3e4db452b660a186bd2a18d17a693b
Breno-SV <brenozildox@gmail.com>
2026-07-18 14:07:40 -0300

    Case A Completed

--- script.py
+++ script.py
@@ -1,35 +1,51 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 
-# Carregar toda a  planilha
-df = pd.read_excel("EPIC_Boston_2026_Participants.xlsx")
+# Loading the entire spreadsheet
+df = pd.read_excel("Participants_spreadsheet/EPIC_Boston_2026_Participants.xlsx")
 
-# Carregar template exportado como imagem
-template = Image.open("Epic_badgeMain.png")
+# Load templates
+template_badge = Image.open("Template_badge/Epic_badgeMain.png")
+template_certificate = Image.open("Template_certificate/Template_certificate_main.png")
 
-# Definir fontes
-fonte_nome = ImageFont.truetype("Fonts/Montserrat-Bold.ttf", 63)
-fonte_empresa = ImageFont.truetype("Fonts/Montserrat-Bold.ttf", 40)
+# Define fonts
+fonte_name_badge = ImageFont.truetype("Text_Fonts/Montserrat-Bold.ttf", 63)
+fonte_company_badge = ImageFont.truetype("Text_Fonts/Montserrat-Bold.ttf", 40)
 
-# Loop pelos participantes
-for _, linha in df.iterrows():
-    nome = linha["Full Name"]
-    empresa = linha["Company"]
+# Specific font for the certificate
+fonte_cert_nome = ImageFont.truetype("Text_Fonts/GreatVibes-Regular.ttf", 160)
 
-# Extrair primeiro e último nome
-    partes_nome = nome.split()
-    first_name = partes_nome[0]
-    last_name = partes_nome[-1]
-    file_name = f"Badge_{first_name}_{last_name}.png"
+# Color black and teal (#09ABA3)
+teal = (9, 171, 163)
 
-# Copiar template
-    credencial = template.copy()
-    draw = ImageDraw.Draw(credencial)
+for _, line in df.iterrows():
+    nome = line["Full Name"]
+    empresa = line["Company"]
 
-# Inserir textos (ajuste coordenadas conforme layout)
-    draw.text((250, 1050), nome, font=fonte_nome, fill="black")
-    draw.text((250, 1350), f"{empresa}", font=fonte_empresa, fill="black")
+    parts_name = nome.split()
+    first_name = parts_name[0]
+    last_name = parts_name[-1]
 
-# Salvar credencial
-    credencial.save(f"Badges/{file_name}")
+    badge_name = f"Badge_{first_name}_{last_name}.png"
+    certificate_name = f"Certificate_{first_name}_{last_name}.pdf"
+
+#---Badge---
+    badge = template_badge.copy()
+    draw_badge = ImageDraw.Draw(badge)
+    draw_badge.text((250, 1050), nome, font=fonte_name_badge, fill="black")
+    draw_badge.text((250, 1350), empresa, font=fonte_company_badge, fill="black")
+    badge.save(f"Epic_Badges/{badge_name}")
+
+#---Certificate---
+    cert = template_certificate.copy()
+    draw_cert = ImageDraw.Draw(cert)
+    draw_cert.text(
+        (1754, 1305), #
+        nome,
+        font=fonte_cert_nome,
+        fill="teal",
+        anchor="mm"
+    )
+    cert.save(f"Epic_Certificates/{certificate_name}", "PDF")
+
 
